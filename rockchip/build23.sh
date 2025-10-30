1dd18370790874ad07b970984af27c4d46cafd8e
泉户琥珀 <42205627+mitokohaku@users.noreply.github.com>
2025-10-30 09:28:50 +0800

    Update build23.sh

--- rockchip/build23.sh
+++ rockchip/build23.sh
@@ -50,6 +50,9 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
 # 定义所需安装的包列表 23.05.4 下列插件你都可以自行删减
 PACKAGES=""
+PACKAGES="$PACKAGES kmod-mt7921e"
+PACKAGES="$PACKAGES kmod-mt7922-firmware"
+PACKAGES="$PACKAGES wpad-openssl"
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
@@ -59,10 +62,9 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 代理工具
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
